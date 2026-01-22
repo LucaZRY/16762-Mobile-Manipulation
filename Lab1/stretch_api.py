82fc876d38d98b1632bd73ec7b827de4b2ff005b
Lucas Zhou <lucaszhoury@gmail.com>
2026-01-22 16:06:28 -0500

    lab1

--- Lab1/stretch_api.py
+++ Lab1/stretch_api.py
@@ -10,70 +10,68 @@ def main():
     robot = stretch_body.robot.Robot()
     robot.startup()
 
-    try:
-        robot.stow()
-        robot.push_command()
-        safe_sleep(3.0)
+    robot.stow()
+    robot.push_command()
+    safe_sleep(3.0)
 
-        robot.arm.move_to(0.5)      # full extension (per lecture example)
-        robot.lift.move_to(1.0)     # "all the way up" (often near max; adjust if your robot saturates earlier)
-        robot.push_command()
+    robot.arm.move_to(0.5)      # full extension (per lecture example)
+    robot.lift.move_to(1.0)     # "all the way up" (often near max; adjust if your robot saturates earlier)
+    robot.push_command()
 
 
-        robot.arm.wait_until_at_setpoint()
-        robot.lift.wait_until_at_setpoint()
-        safe_sleep(0.5)
+    robot.arm.wait_until_at_setpoint()
+    robot.lift.wait_until_at_setpoint()
+    safe_sleep(0.5)
 
-        robot.end_of_arm.move_to('wrist_yaw', np.radians(30))
-        robot.push_command()
-        safe_sleep(2.0)
+    robot.end_of_arm.move_to('wrist_yaw', np.radians(30))
+    robot.push_command()
+    safe_sleep(2.0)
 
-        robot.end_of_arm.move_to('wrist_pitch', np.radians(30))
-        robot.push_command()
-        safe_sleep(2.0)
+    robot.end_of_arm.move_to('wrist_pitch', np.radians(30))
+    robot.push_command()
+    safe_sleep(2.0)
 
-        robot.end_of_arm.move_to('wrist_roll', np.radians(30))
-        robot.push_command()
-        safe_sleep(2.0)
+    robot.end_of_arm.move_to('wrist_roll', np.radians(30))
+    robot.push_command()
+    safe_sleep(2.0)
 
-        robot.end_of_arm.move_to('stretch_gripper', 50)  # open (visible)
-        robot.push_command()
-        safe_sleep(2.0)
+    robot.end_of_arm.move_to('stretch_gripper', 50)  # open (visible)
+    robot.push_command()
+    safe_sleep(2.0)
 
-        robot.end_of_arm.move_to('stretch_gripper', 0)   # close
-        robot.push_command()
-        safe_sleep(2.0)
+    robot.end_of_arm.move_to('stretch_gripper', 0)   # close
+    robot.push_command()
+    safe_sleep(2.0)
 
-        robot.head.move_by('head_pan', np.radians(45))
-        robot.push_command()
-        safe_sleep(2.0)
+    robot.head.move_by('head_pan', np.radians(45))
+    robot.push_command()
+    safe_sleep(2.0)
 
-        robot.head.move_by('head_tilt', np.radians(30))
-        robot.push_command()
-        safe_sleep(2.0)
+    robot.head.move_by('head_tilt', np.radians(30))
+    robot.push_command()
+    safe_sleep(2.0)
 
-        robot.stow()
-        robot.push_command()
-        safe_sleep(4.0)
+    robot.stow()
+    robot.push_command()
+    safe_sleep(4.0)
 
-        robot.base.translate_by(0.5)
-        robot.push_command()
-        safe_sleep(5.0)
+    robot.base.translate_by(0.5)
+    robot.push_command()
+    safe_sleep(5.0)
 
-        robot.base.rotate_by(np.radians(180))
-        robot.push_command()
-        safe_sleep(5.0)
+    robot.base.rotate_by(np.radians(180))
+    robot.push_command()
+    safe_sleep(5.0)
 
-        robot.base.translate_by(0.5)
-        robot.push_command()
-        safe_sleep(5.0)
+    robot.base.translate_by(0.5)
+    robot.push_command()
+    safe_sleep(5.0)
+        
+    robot.stow()
+    robot.push_command()
+    safe_sleep(3.0)
 
-        robot.stow()
-        robot.push_command()
-        safe_sleep(3.0)
-
-    finally:
-        robot.stop()
+    robot.stop()
 
 if __name__ == "__main__":
     main()
