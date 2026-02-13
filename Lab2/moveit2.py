a34415b57f493f648a1ddd0f000b80d14b3889fc
Lucas Zhou <lucaszhoury@gmail.com>
2026-02-13 15:53:36 -0500

    1

--- Lab2/moveit2.py
+++ Lab2/moveit2.py
@@ -47,7 +47,7 @@ class MoveMe(HelloNode):
             elif i == 1:
                 # Step 2: Turn left 90 degrees (pi/2 radians)
                 goal_state.set_joint_group_positions(planning_group, 
-                    [0.0, 0.0, -np.pi/2,  # Stay at x=0.2, rotate 90 degrees left
+                    [0.4, 0.0, -np.pi/2,  # Stay at x=0.2, rotate 90 degrees left
                     self.get_joint_pos('joint_lift'), 
                     self.get_joint_pos('joint_arm_l3'), 
                     self.get_joint_pos('joint_arm_l2'), 
@@ -62,7 +62,7 @@ class MoveMe(HelloNode):
                 # Step 3: Go straight forward 0.2m (in the new direction)
                 # After turning left, "forward" is now in the +y direction
                 goal_state.set_joint_group_positions(planning_group, 
-                    [0.0, 0.4, -np.pi/2,  # Move forward 0.2m in new direction (increases y)
+                    [0.4, 0.0, -np.pi/2,  # Move forward 0.2m in new direction (increases y)
                     self.get_joint_pos('joint_lift'), 
                     self.get_joint_pos('joint_arm_l3'), 
                     self.get_joint_pos('joint_arm_l2'), 
